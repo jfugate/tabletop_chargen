49861b3118be328aa2e70bf3d9b9aaf2f2ac466a
jfugate <jeremycfugate@gmail.com>
2017-03-14 19:05:52 -0400

    updated menu with new character name saver, new vars for checksums later

---
+++ char_name.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+"""This simple script will return the value of whatever name the player chooses"""
+
+def name_saver():
+	name=raw_input("Please Enter character name: ")
+	return name
