0f77b4371741ca8913b444871f4b40a7a7cafcd5
jfugate <jeremycfugate@gmail.com>
2017-03-14 16:17:26 -0400

    starting the main menu function

---
+++ menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#This script will form the menus, and call the functions that both build and store relevant information, then display it to the user
+
+def main_menu():
+	menu = {}
+	menu['1']="Pick a Name"
+	menu['2']="Pick a Race"
+	menu['3']="Pick a Class"
+	menu['4']="Pick an Alignment"
+	menu['5']="Roll Stats"
+	menu['6']="Build Sheeet"
