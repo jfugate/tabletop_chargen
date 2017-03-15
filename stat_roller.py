0d4e41a4955e58fb63decab408a06cfe1e469ec2
jfugate <jeremycfugate@gmail.com>
2017-03-15 15:16:17 -0400

    testing stat roller, moving functions around for better operation

--- stat_roller.py
+++ stat_roller.py
@@ -4,52 +4,58 @@
    values of the rolls to the corresponding character ability"""
 
 import random
+def assign_scores(my_list):
+	print("Here are the scores: ", my_list)
+	strength=raw_input("Please input the value for strength: ")
+	my_list.remove(int(strength))
+	dexterity=raw_input("Please input the value for dexterity: ")
+	my_list.remove(int(dexterity))
+	constiution=raw_input("Please input the value for constitution: ")
+	my_list.remove(int(constitution))
+	intelligence=raw_input("Please input the value for intelligence: ")
+	my_list.remove(int(intelligence))
+	wisdom=raw_input("Please input the value for wisdom: ")
+	my_list.remove(int(wisdom))
+	charisma=raw_input("Please input the value for charisma: ")
+	my_list.remove(int(charisma))
+	""" will probably build a dictionary as the return object that goes back to the main script.
+	    this is likely the easiest way to handle the data
+	    for testing only I will print each variable separately"""
+	print strength, ' ', dexterity, ' ', constitution, ' ', intelligence, ' ', wisdom, ' ', charisma
 
 def scores():
 	score_menu={}
 	score_menu['1']="Traditional D6 method"
 	score_menu['2']="Patton's Patented D12 Method*TM"
 
-	print("Please choose a rolling method below:")
+	
 	options=score_menu.keys()
 	options.sort()
-
+	print("Please choose a rolling method below:")
 	for entry in options:
-		print entry, options[entry]
+		print(entry, score_menu[entry])
 
 	roll_method=raw_input("Method choide: ")
+	print(roll_method)
 
-	if roll_method == 1:
+	if roll_method == '1':
 		print("Traditional Method selected! Rolling up 6 random numbers")
 		score_range=range(3, 18)
 		score_set=[]
-		for i in range(1, 6):
+		for i in range(0, 6):
 			score_set.append(random.choice(score_range))
 		print("Scores generated, now to assign them, keep in mind what class you might choose!")
 		assign_scores(score_set)
-	elif roll_method == 2:
+	elif roll_method == '2':
 		print("Patton's D12 method selected! Rolling up 6 random numbers")
 		score_range=range(9, 20)
 		score_set=[]
-		for i in range(1, 6):
+		for i in range(0, 6):
 			score_set.append(random.choice(score_range))
 		print("Scores generated, now to assign them, keep in mind what class you might choose!")
 		assign_scores(score_set)
+	else:
+		print("Invalid choice")
+		scores()
 
-
-	def assign_scores(my_list):
-		print("Here are the scores: ", array_dump(my_list))
-		strength=raw_input("Please input the value for strength: ")
-		my_list.remove(strength)
-		dexterity=raw_input("Please input the value for dexterity: ")
-		my_list.remove(dexterity)
-		constiution=raw_input("Please input the value for constitution: ")
-		my_list.remove(constitution)
-		intelligence=raw_input("Please input the value for intelligence: ")
-		my_list.remove(intelligence)
-		wisdom=raw_input("Please input the value for wisdom: ")
-		my_list.remove(wisdom)
-		charisma=raw_input("Please input the value for charisma: ")
-		my_list.remove(charisma)
-		""" will probably build a dictionary as the return object that goes back to the main script.
-		    this is likely the easiest way to handle the data"""
+scores()
